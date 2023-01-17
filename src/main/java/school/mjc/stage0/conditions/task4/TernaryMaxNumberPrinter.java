08b6a93e20e2e3801956e1b3193ba4bda8e98568
Mirakbar Mirzokhidov <mirakbarmirzokhidov21@gmail.com>
2023-01-17 23:21:30 +0600

    upd: ternary operator

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int maxx;
+        maxx = first > second ? (maxx = first > third ? first: third): (maxx = second > third ? second: third);
+        System.out.println(maxx);
     }
-}
+}// 1 2 3
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
+        String max;
+        System.out.println(max = first > second ? "first": "second");
     }
 }
